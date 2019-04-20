a13ac25781192b08a09af8d0a0d4240d1e20e6f6
Ben Mitchinson <115bwm@gmail.com>
2019-04-20 17:50:00 -0500

    Row SmallStats from State and Events Endpoint

---
+++ NeededRoutes.md
@@ -0,0 +1,12 @@
+# Needed Routes
+
+All info returned as JSON
+
+* `/Events` gives all event documents (Pagenation?)
+* `/Events/?Date` if a date is provided to the AllEvents route, return event documents from that date alone
+* `/Events/?StartDate?EndDate` if a start and end date is provided, return event documents from start to end date
+* `/Events/?User` if a user is provided, return all events from that user
+* ``
+* ``
+* ``
+* ``
--- react-ui/src/data/sidebar-nav-items.js
+++ react-ui/src/data/sidebar-nav-items.js
@@ -1,25 +1,10 @@
 export default function() {
   return [
     {
-      title: "Blog Dashboard",
+      title: "Dashboard",
       to: "/blog-overview",
-      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlBefore: '<i class="material-icons">pie_chart</i>',
       htmlAfter: ""
-    },
-    {
-      title: "Blog Posts",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
-      to: "/blog-posts",
-    },
-    {
-      title: "Add New Post",
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: "/add-new-post",
-    },
-    {
-      title: "Forms & Components",
-      htmlBefore: '<i class="material-icons">view_module</i>',
-      to: "/components-overview",
     },
     {
       title: "Tables",
@@ -30,11 +15,6 @@ export default function() {
       title: "User Profile",
       htmlBefore: '<i class="material-icons">person</i>',
       to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
     }
   ];
 }
