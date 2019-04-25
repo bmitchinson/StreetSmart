459fd386e20d1e6cd801eb0ae8437590aeba99ec
Ben Mitchinson <115bwm@gmail.com>
2019-04-24 23:33:09 -0500

    Working demo script, no Firestore yet

--- server/SimulateTrip.js
+++ server/SimulateTrip.js
@@ -13,9 +13,6 @@ var db = firebase.firestore();
 let Battery = 81 // Will decay slowly
 let Driver = "Tyler" // Constant
 
-// Alters varience from speed limit
-let DriveStyle = 1 // -1: Slow driver, 0: Strict Driver, 1: Late, 2: Stupid
-
 // Optional status code enabled for the entire trip
 let StatusCode = ""
 
@@ -24,14 +21,18 @@ let Time = moment('04-24-2019 19:53', 'MM-DD-YYYY hh:mm');
 
 // check point strings to use for simulated trip
 let checkpointStrings = [
-  "41.660058, -91.526004",
-  "41.660090, -91.519642",
-  "41.653349, -91.509965"
+  "41.660145, -91.537679",
+  "41.659920, -91.520041",
+  "41.651764, -91.507637"
 ]
+// Alters varience from speed limit
+let DriveStyle = 0 // -2: Slow driver, 0: Strict Driver, 1: Late, 3: Stupid
+
 let speedLimit = 35
-let sampleRate = 5 // Sample every 15 seconds
-let delayMode = false
+let sampleRate = 15 // Sample every 15 seconds
+let timeFactor = 3
 let pushMode = false
+let delayMode = true
 
 // Converts strings to GeoPoint
 let checkpoints = []
@@ -42,59 +43,94 @@ checkpointStrings.forEach( (text) => {
   checkpoints.push(geo);
 })
 
+function cordDistKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 let carGeoPoint = checkpoints[0];
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 console.log(Time.format('lll') + ": Starting 🚗  @ " 
-+ String(carGeoPoint.latitude) + ", " + String(carGeoPoint.longitude))
++ String(carGeoPoint.latitude) + ", " + String(carGeoPoint.longitude) + 
+" Driver Style: " + DriveStyle)
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 // Begin adding data in simulated time (if delayMode, otherwise all at once)
 checkpoints.forEach((destPoint, index) => {
-  // Variance in speed
-  let carMPH = speedLimit + (DriveStyle*Math.floor(Math.random()*10)+1)
-  let carMPS = carMPH / 3600
-  console.log(Time.format('lll') + ": 🚗  located @ " + 
-  String(carGeoPoint.latitude) + ", " + String(carGeoPoint.longitude) + " going "
-    + carMPH + "MPH in a " + speedLimit + "MPH Zone");
-  let carMPSample = carMPS * sampleRate
-  let milesBetween = 0;
-  let subPointsCount = milesBetween / carMPSample
-  let subPoints = [];
-  let latShift = (destPoint.latitude - 
-  carGeoPoint.latitude)/subPointsCount
-  let longShift = (destPoint.longitude - 
-  carGeoPoint.longitude)/subPointsCount
-  let subPointsIndex = 0;
-  while (subPointsIndex < subPointsCount-1){
-    // Shift + push to FB
-    carGeoPoint = new firebase.firestore.GeoPoint(
-      (carGeoPoint.latitude + latShift),
-      (carGeoPoint.longitude + longShift)
-    )
-    if (pushMode){
-      console.log("Give to FB")
+  if (index != 0) {
+    // Variance in speed
+    let carMPH = speedLimit + (DriveStyle*Math.floor(Math.random()*10)+1)
+    if (DriveStyle == 0){
+      carMPH--
     }
-    subPointsIndex++
-    if (delayMode){
-      setTimeout(sampleRate * 1000);
+    let carMPS = carMPH / 3600
+    let carMPSample = carMPS * sampleRate
+    let milesBetween = cordDistKm(destPoint.latitude, destPoint.longitude,
+      carGeoPoint.latitude, carGeoPoint.longitude) * .6214; //KM to Miles
+    let subPointsCount = milesBetween / carMPSample
+    let subPoints = [];
+    let latShift = (destPoint.latitude - 
+      carGeoPoint.latitude)/subPointsCount
+    let longShift = (destPoint.longitude - 
+      carGeoPoint.longitude)/subPointsCount
+    latShift = latShift
+    longShift = latShift
+    let subPointsIndex = 0;
+    while (subPointsIndex < subPointsCount-1){
+      // Shift + push to FB
+      carGeoPoint = new firebase.firestore.GeoPoint(
+        carGeoPoint.latitude + latShift,
+        carGeoPoint.longitude + longShift
+        )
+        if (pushMode){
+          console.log("Give to FB")
+        }
+        if (delayMode){
+          sleep(1000 * sampleRate / timeFactor);
+        }
+        subPointsIndex++
+        Time = Time.add(sampleRate, 'seconds')
+        console.log(Time.format('lll') + ": 🚗  located @ " + 
+          String(Math.round(carGeoPoint.latitude * 1000000.0) / 1000000.0) + 
+          ", " + String(Math.round(carGeoPoint.longitude * 1000000.0) / 1000000.0)
+          + " going " + carMPH + "MPH in a " + speedLimit + "MPH Zone"
+        );
+      }
+      carGeoPoint = checkpoints[index]
+      if (pushMode){
+        console.log("Give to FB")
+      }
+      if (delayMode){
+        sleep(1000 * sampleRate / timeFactor);
+      }
+      Time = Time.add(sampleRate, 'seconds')
+      console.log(Time.format('lll') + ": 🚗  reached point " + index + "! " + checkpointStrings[index]);
     }
-    Time = Time.add(sampleRate, 'seconds')
-    console.log(Time.format('lll') + ": 🚗 located @ " + carGeoPoint + " going "
-    + carMPH + "MPH in a " + speedLimit + "MPH Zone");
-  }
-  carGeoPoint = checkpoints[index]
-  if (pushMode){
-    console.log("Give to FB")
-  }
-  if (delayMode){
-    setTimeout(sampleRate * 1000);
-  }
-  Time = Time.add(sampleRate, 'seconds')
-  console.log(Time.format('lll') + ": 🚗  reached point " + index + "!");
-})
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.log("End of trip")
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
+  })
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  console.log("End of trip");
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 /* db.collection("events").doc().set({
   Battery: Battery,
