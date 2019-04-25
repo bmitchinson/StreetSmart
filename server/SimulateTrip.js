e77c40f320d98dee7c07fbc769b0ca17d861b795
Ben Mitchinson <115bwm@gmail.com>
2019-04-24 22:24:43 -0500

    Simulate Almost Done with corrected route key names

---
+++ server/SimulateTrip.js
@@ -0,0 +1,111 @@
+const firebase = require("firebase");
+var moment = require('moment');
+
+// Firebase initializtion
+firebase.initializeApp({
+  apiKey: "***REMOVED***",
+  authDomain: "streetsmart-2cf68.firebaseapp.com",
+  projectId: "streetsmart-2cf68"
+});
+var db = firebase.firestore();
+
+// Initial Values for simulating a "trip"
+let Battery = 81 // Will decay slowly
+let Driver = "Tyler" // Constant
+
+// Alters varience from speed limit
+let DriveStyle = 1 // -1: Slow driver, 0: Strict Driver, 1: Late, 2: Stupid
+
+// Optional status code enabled for the entire trip
+let StatusCode = ""
+
+// Starting time of the trip
+let Time = moment('04-24-2019 19:53', 'MM-DD-YYYY hh:mm');
+
+// check point strings to use for simulated trip
+let checkpointStrings = [
+  "41.660058, -91.526004",
+  "41.660090, -91.519642",
+  "41.653349, -91.509965"
+]
+let speedLimit = 35
+let sampleRate = 5 // Sample every 15 seconds
+let delayMode = false
+let pushMode = false
+
+// Converts strings to GeoPoint
+let checkpoints = []
+
+checkpointStrings.forEach( (text) => {
+  let geo = new firebase.firestore.GeoPoint(
+    Number(text.split(',')[0]), Number(text.split(',')[1]));
+  checkpoints.push(geo);
+})
+
+let carGeoPoint = checkpoints[0];
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log(Time.format('lll') + ": Starting 🚗  @ " 
++ String(carGeoPoint.latitude) + ", " + String(carGeoPoint.longitude))
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// Begin adding data in simulated time (if delayMode, otherwise all at once)
+checkpoints.forEach((destPoint, index) => {
+  // Variance in speed
+  let carMPH = speedLimit + (DriveStyle*Math.floor(Math.random()*10)+1)
+  let carMPS = carMPH / 3600
+  console.log(Time.format('lll') + ": 🚗  located @ " + 
+  String(carGeoPoint.latitude) + ", " + String(carGeoPoint.longitude) + " going "
+    + carMPH + "MPH in a " + speedLimit + "MPH Zone");
+  let carMPSample = carMPS * sampleRate
+  let milesBetween = 0;
+  let subPointsCount = milesBetween / carMPSample
+  let subPoints = [];
+  let latShift = (destPoint.latitude - 
+  carGeoPoint.latitude)/subPointsCount
+  let longShift = (destPoint.longitude - 
+  carGeoPoint.longitude)/subPointsCount
+  let subPointsIndex = 0;
+  while (subPointsIndex < subPointsCount-1){
+    // Shift + push to FB
+    carGeoPoint = new firebase.firestore.GeoPoint(
+      (carGeoPoint.latitude + latShift),
+      (carGeoPoint.longitude + longShift)
+    )
+    if (pushMode){
+      console.log("Give to FB")
+    }
+    subPointsIndex++
+    if (delayMode){
+      setTimeout(sampleRate * 1000);
+    }
+    Time = Time.add(sampleRate, 'seconds')
+    console.log(Time.format('lll') + ": 🚗 located @ " + carGeoPoint + " going "
+    + carMPH + "MPH in a " + speedLimit + "MPH Zone");
+  }
+  carGeoPoint = checkpoints[index]
+  if (pushMode){
+    console.log("Give to FB")
+  }
+  if (delayMode){
+    setTimeout(sampleRate * 1000);
+  }
+  Time = Time.add(sampleRate, 'seconds')
+  console.log(Time.format('lll') + ": 🚗  reached point " + index + "!");
+})
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("End of trip")
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+/* db.collection("events").doc().set({
+  Battery: Battery,
+  Driver: Driver,
+  Location: Location,
+  Speed: Speed,
+  SpeedLimit: SpeedLimit,
+  SpeedStatus: SpeedStatus,
+  StatusCode: StatusCode,
+  Time: firebase.firestore.Timestamp.fromDate(Time.toDate())
+}).then( () => {
+  console.log("Wrote to firebase");
+  process.exit();
+}) */
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const http = require('http')
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-// const Firestore = require('@google-cloud/firestore');
 const firebase = require("firebase");
 var moment = require('moment');
 
@@ -16,17 +15,6 @@ firebase.initializeApp({
 });
 
 var db = firebase.firestore();
-
-// db.collection("events").get().then((querySnapshot) => {
-//   querySnapshot.forEach((doc) => {
-//       let time = doc.data().Time
-//       console.log(time)
-//       let date = moment(time);
-//       date = date.toDate();
-//       console.log(date.format("MM/DD/YY"))
-//   });
-// });
-
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
@@ -50,7 +38,7 @@ if (!isDev && cluster.isMaster) {
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  // Answer API requests.
+  // Dump all data when called
   app.get('/api', function (req, res) {
     let hash = []
     db.collection("events").get().then((querySnapshot) => {
@@ -63,33 +51,14 @@ if (!isDev && cluster.isMaster) {
 
   // Route for getting all events
   app.get('/Events', function (req, res) {
-    // if(req.query.date === undefined) {
-    //   let hash = []
-    //   db.collection("events").get().then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //         hash.push(doc.data())
-    //     });
-    //     res.json(hash)
-    //   });
-    // } 
-    // else {
-    //   let date_from_url = req.query.date
-    //   let date = moment(date_from_url).toDate()
-    //   // console.log(date)
-    //   let hash = []
-    //   db.collection("events").where("Time","==",date).get().then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //       hash.push(doc.data())
-    //     });
-    //     res.json(hash)
-    //   });
-    // }
     let query = db.collection("events")
+
     if(req.query.Date !== undefined) {
       let date_from_url = req.query.Date
       let date = moment(date_from_url).toDate()
       query = query.where("Time", "==", date)
     }
+
     if(req.query.StartDate !== undefined) {
       let start_date_from_url = req.query.StartDate
       let end_date_from_url = req.query.EndDate
@@ -97,24 +66,26 @@ if (!isDev && cluster.isMaster) {
       let end_date = moment(end_date_from_url).toDate()
       query = query.where("Time", ">=", start_date).where("Time", "<=", end_date)
     }
+
     if(req.query.Driver !== undefined) {
       query = query.where("Driver", "==", req.query.Driver)
     }
+
     if(req.query.Safe !== undefined) {
-      query = query.where("Speeding-Status", "<=", 0)
+      query = query.where("SpeedStatus", "<=", 0)
     }
+
     if(req.query.Speeding !== undefined) {
-      query = query.where("Speeding-Status", ">", 0)
+      query = query.where("SpeedStatus", ">", 0)
     }
    
     let hash = []
     query.get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-              hash.push(doc.data())
-          });
-          res.json(hash)
-        });
-
+      querySnapshot.forEach((doc) => {
+          hash.push(doc.data())
+      });
+      res.json(hash)
+    });
   });
 
   // All remaining requests return the React app, so it can handle routing.
