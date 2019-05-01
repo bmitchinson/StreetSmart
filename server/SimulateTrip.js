669cbfb7f0dff3635b20a86e0aa8f5896ece94de
Ben Mitchinson <115bwm@gmail.com>
2019-05-01 02:45:11 -0500

    Working live update heat map, working on origin location

--- server/SimulateTrip.js
+++ server/SimulateTrip.js
@@ -17,7 +17,7 @@ let StatusCode = ""
 
 // Starting time of the trip
 let Driver = "Tyler" // Constant
-let Time = moment('04-02-2019 10:45', 'MM-DD-YYYY hh:mm');
+let Time = moment('05-14-2019 10:45', 'MM-DD-YYYY hh:mm');
 
 // to use for simulated trip
 let checkpointStrings = [
@@ -36,8 +36,8 @@ let speedLimits = [
 ]
 
 // Alters varience from speed limit
-let DriveStyle = -1 // -2: Slow driver, 0: Strict Driver, 1: Late, 3: Stupid
-let sampleRate = 15 // Sample every 15 seconds
+let DriveStyle = 2 // -2: Slow driver, 0: Strict Driver, 1: Late, 3: Stupid
+let sampleRate = 8 // Sample every 15 seconds
 let timeFactor = 2 // Move time along 3x times faster
 let pushMode = true // If enabled results are pushed to firebase
 let delayMode = true // If enabled, time / speed MPH accurate delays occur
